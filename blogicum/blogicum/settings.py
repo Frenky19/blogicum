8bc67b0b1c7613a484a994d4b987985a94d88d3c
Andrey Golovushkin <frenkodetto@yandex.ru>
2025-02-04 17:27:16 +0300

    1

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -32,7 +32,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 INTERNAL_IPS = [
